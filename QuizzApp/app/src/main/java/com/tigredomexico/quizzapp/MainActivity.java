853dfffef7d19a9a21b19044626c1cf7efc5fae3
David Tigre Moraes <tigredavid007@gmail.com>
2023-08-31 16:35:43 -0300

    Conexão com outra tela

--- QuizzApp/app/src/main/java/com/tigredomexico/quizzapp/MainActivity.java
+++ QuizzApp/app/src/main/java/com/tigredomexico/quizzapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.tigredomexico.quizzapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.iniciar_button).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, QuizzActivity.class);
+            startActivity(intent);
+        });
     }
 }
